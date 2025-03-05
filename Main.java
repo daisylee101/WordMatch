88f24701acb36bd364c867e01eaae54a3cbf3f1f
daisylee101 <daisyl43@nycstudents.net>
2025-03-05 17:33:58 +0000

    Created 3.5

---
+++ Main.java
@@ -0,0 +1,22 @@
+public class WordMatch { 
+
+private String secret;
+
+public WordMatch(String word) { }
+
+public int scoreGuess(String guess) {
+    for (i = 0; i < secret.length(); i++;) { 
+        String guess1;
+        int count;
+        String check = secret.substring(i,i+1);
+        if guess.contains(check) {
+            count
+        }
+       
+        }
+        return count;
+    }
+} 
+
+public String findBetterGuess(String guess1, String guess2){} 
+} 
